e71b94813865191fe4e755486aaacd26439b58d0
David Chung <dchung@dchung.com>
2020-05-06 02:45:17 -0500

    updated review data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,24 @@
 insert into review (review_text, rating, restaurant_id) values('Meat! Need I say more?', 5, 1);
-insert into review (review_text, rating, restaurant_id) values('Are you serious? I am vegan!', 5, 1);
+insert into review (review_text, rating, restaurant_id) values('Are you serious? I am vegan!', 4, 1);
+insert into review (review_text, rating, restaurant_id) values('Even the salad bare is awesome.', 5, 1);
+
+insert into review (review_text, rating, restaurant_id) values('The Aji Tataki was a show stopper!.', 5, 2);
+insert into review (review_text, rating, restaurant_id) values('I do not know what the hype is about.', 2, 2);
+insert into review (review_text, rating, restaurant_id) values('Not bad, but nothing special.', 3, 2);
+
+insert into review (review_text, rating, restaurant_id) values('I loved the mango flan.', 5, 3);
+insert into review (review_text, rating, restaurant_id) values('Food was great but the service was slow.', 4, 3);
+insert into review (review_text, rating, restaurant_id) values('My paella was cold.', 2, 3);
+
+insert into review (review_text, rating, restaurant_id) values('First rate experience from the time we walked in the door.', 5, 4);
+insert into review (review_text, rating, restaurant_id) values('Overpriced, still probably worth it ar lunchtime.', 3, 4);
+insert into review (review_text, rating, restaurant_id) values('Paolo was a great waiter!', 4, 4);
+
 insert into review (review_text, rating, restaurant_id) values('Fresh, authentic and creative. The Blowfish is to die for!', 5, 5);
-insert into review (review_text, rating, restaurant_id) values('A review', 3, 4);
-insert into review (review_text, rating, restaurant_id) values('A review', 3, 7);
-insert into review (review_text, rating, restaurant_id) values('A review', 4, 8);
-insert into review (review_text, rating, restaurant_id) values('A review', 5, 9);
-insert into review (review_text, rating, restaurant_id) values('A review', 2, 10);
-insert into review (review_text, rating, restaurant_id) values('A review', 3, 11);
-insert into review (review_text, rating, restaurant_id) values('A review', 3, 12);
-insert into review (review_text, rating, restaurant_id) values('A review', 5, 7);
-insert into review (review_text, rating, restaurant_id) values('A review', 3, 8);
-insert into review (review_text, rating, restaurant_id) values('A review', 1, 9);
-insert into review (review_text, rating, restaurant_id) values('A review', 2, 10);
-insert into review (review_text, rating, restaurant_id) values('A review', 3, 11);
-insert into review (review_text, rating, restaurant_id) values('A review', 3, 12);
+insert into review (review_text, rating, restaurant_id) values('Adventure dining at its finest.', 5, 5);
+insert into review (review_text, rating, restaurant_id) values('If the blowfish sashimi does not kill you - you wil love this place.', 5, 5);
+
+insert into review (review_text, rating, restaurant_id) values('Bacon wrapped dates are my favorite!', 5, 6);
+insert into review (review_text, rating, restaurant_id) values('The octopus was over cooked.', 3, 6);
+insert into review (review_text, rating, restaurant_id) values('Good choices, good quality ... I will be back.', 4, 6);
+
